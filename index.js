a9c3e659fb9449195ecfcf46c9a7fd15a3116d11
WhosamCR <Houssemhsm@yahoo.fr>
2021-06-18 19:22:27 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+// //alert("Do not procrastinate you fool!");
+// console.log("I hope this works!");
+
+// //Comment
+
+// /* 
+
+// This is a 
+// multi-line comment
+
+// */
+// var age = 24; 
+// var firstName = "Houssem";
+// var lastName = "Cheikh-Rouhou";
+// var online = true;
+// var whatever = null;
+
+// age = age + 1;
+// var fullName = firstName +" "+ lastName;
+
+
+
+// console.log("I am "+age, "years old");
+// console.log("Hello",firstName);
+// console.log ("Are you online? :",online);
+// console.log(whatever);
+// console.log("Happy", age+"th birthday",fullName);
+
+// var myName = window.prompt("Insert your name");
+// console.log("Hello", myName);
+
+document.getElementById("myButton").onclick = function(){
+
+    var myName = document.getElementById("myText").value;
+    console.log("Hello",myName);
+}
